8f979b5ff250ea0ddebc5b2d2f65429a20a4c9b0
Mustofa Mahmud <mustofa.mahmud@bjitgroup.com>
2024-08-30 10:03:37 +0600

    new comment added

--- src/main/java/com/example/ci_demo_psd/CiDemoPsdApplication.java
+++ src/main/java/com/example/ci_demo_psd/CiDemoPsdApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CiDemoPsdApplication {
 
 	public static void main(String[] args) {
+		//test commit
 		SpringApplication.run(CiDemoPsdApplication.class, args);
 	}
 
